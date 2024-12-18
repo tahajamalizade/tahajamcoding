89c70d06c0aac20d128f356128ac3bb10e6e77f7
taha <mohammad.taha.jam@gmail.com>
2024-12-18 13:43:52 +0330

    initial commit

--- src/components/ProductCart.vue
+++ src/components/ProductCart.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="card" v-for="product in products" :key="product.id">
     <img
-      src="../assets/propic.jpg"
-      alt="Denim Jeans"
-      style="width: 80%; padding: 0px 30px; border-radius: 15px; padding: 20px"
+      :src="product.image"
+      :alt="product.name"
+      style="width: 70%; padding: 0px 30px; border-radius: 15px; padding: 20px"
     />
-    <ul class="list">
-      <li>
-        <h4>{{ product.name }}</h4>
-        <h5>{{ product.price }}</h5>
-      </li>
-    </ul>
+    <div class="info">
+      <ul class="list">
+        <li>
+          <h4>{{ product.name }}</h4>
+          <h5>{{ product.price }}</h5>
+        </li>
+      </ul>
+    </div>
 
     <div style="left: 20px">
       <p><button>Add to Cart</button></p>
@@ -21,13 +23,37 @@
 export default {
   data() {
     return {
-      ShowProducts: true,
       products: [
-        { id: 1, name: "jeans", price: '10$', },
-        { id: 2, name: "Tshirt", price: '20$' },
-        { id: 3, name: "shirt", price: '40$' },
-        { id: 4, name: "gloves", price: '50$' },
-        { id: 5, name: "socks", price: '50$' },
+        {
+          id: 1,
+          name: "jeans",
+          price: "10$",
+          image: "../src/assets/jeans.jpeg",
+        },
+        {
+          id: 2,
+          name: "Tshirt",
+          price: "20$",
+          image: "../src/assets/tshirt.jpeg",
+        },
+        {
+          id: 3,
+          name: "shirt",
+          price: "40$",
+          image: "../src/assets/shirt.jpeg",
+        },
+        {
+          id: 4,
+          name: "gloves",
+          price: "50$",
+          image: "../src/assets/gloves.jpeg",
+        },
+        {
+          id: 5,
+          name: "socks",
+          price: "50$",
+          image: "../src/assets/socks.jpeg",
+        },
       ],
     };
   },
@@ -37,39 +63,41 @@ export default {
 <style lang="scss">
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
+  width: 300px;
+  height: 450px;
   text-align: center;
   font-family: arial;
   background-color: #7fcfa8;
   border-radius: 20px;
   cursor: pointer;
-  height: fit-content;
+  // height: fit-content;
 
-  .list li{
+  .list li {
     list-style-type: none;
-    float: left;
   }
-  .list li h4,h5{
+  .list {
+    padding-left: 0px;
+  }
+  .list li h4,
+  h5 {
     margin: 0;
     color: #4a5c53;
     padding-bottom: 5px;
-
   }
 }
 
 .card button {
   border: none;
-  outline: 0;
-  padding: 12px;
   color: #558b71;
   /* background-color: #000; */
-  text-align: right;
+  text-align: center;
   cursor: pointer;
-  width: 40%;
+  width: 100px;
+  height: 50px;
   font-size: 18px;
   transition: 0.4s;
   border-radius: 20px;
-  float: floor($number: 0);
+  padding: 4px;
 }
 
 .card button:hover {
